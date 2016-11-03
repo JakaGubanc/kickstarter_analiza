aadddd1cb6a8eb9da9151da6497a0d3d68e66de8
Jaka Gubanc <jaka.gubanc@gmail.com>
2016-11-03 10:05:15 +0100

    naredil se eno py datoteko v kateri je samo koda za pridobivanje spltnih strani projektov in za shranjevanje url naslovov teh projktov v tekstovno datoteko. To tekstovno datoteko sm tudi dodal na git.

--- pridobivanje_podatkov.py
+++ pridobivanje_podatkov.py
@@ -1,19 +1,6 @@
 import re
 import orodja
 
-for stran in range(1,5):
-	naslov_glavni = 'https://www.kickstarter.com/discover/advanced?&sort=most_funded'
-	polni_naslov='{}&page={}'.format(naslov_glavni,stran)
-	ime_datoteke = 'strani_seznamov_projektov/{:02}.html'.format(stran)
-	orodja.shrani(polni_naslov,ime_datoteke)
-
-spletni_naslovi_projektov=set()
-for datoteka in orodja.datoteke('strani_seznamov_projektov/'):
-	naslovi_projektov=set()
-	vsebina=orodja.vsebina_datoteke(datoteka)
-	for naslov in re.finditer(r'<a href="/projects/(?P<spletni_naslov>.+)\?ref=most_funded" target=""',vsebina):
-		spletni_naslovi_projektov.add(naslov.groupdict()['spletni_naslov'])
-
 
 stevec=1
 for naslov in spletni_naslovi_projektov:
---
+++ pridobivanje_projektov.py
@@ -0,0 +1,26 @@
+import re
+import orodja
+
+# for stran in range(1,101):
+# 	naslov_glavni = 'https://www.kickstarter.com/discover/advanced?&sort=end_date'
+# 	polni_naslov='{}&page={}'.format(naslov_glavni,stran)
+# 	ime_datoteke = 'strani_seznamov_projektov/{:02}.html'.format(stran)
+# 	orodja.shrani(polni_naslov,ime_datoteke)
+
+spletni_naslovi_projektov=set()
+for datoteka in orodja.datoteke('strani_seznamov_projektov/'):
+	naslovi_projektov=set()
+	vsebina=orodja.vsebina_datoteke(datoteka)
+	for naslov in re.finditer(r'href="/projects/(?P<spletni_naslov>.+)\?.*?',vsebina):
+		spletni_naslovi_projektov.add(naslov.groupdict()['spletni_naslov'])
+
+print(spletni_naslovi_projektov)
+print(len(spletni_naslovi_projektov))
+
+
+with open('spletni_naslovi_projektov', 'w+') as datoteka:
+	for naslov_projekta in spletni_naslovi_projektov:
+		datoteka.write(naslov_projekta+'\n')
+
+   
+
